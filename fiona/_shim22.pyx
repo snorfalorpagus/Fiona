672d1a0c20a42a4a806f619a48da859f574d5c7f
Joshua Arnott <josh@snorfalorpagus.net>
2017-07-13 17:28:05 +0100

    Working in GDAL 2.2

--- fiona/_shim22.pyx
+++ fiona/_shim22.pyx
@@ -2,6 +2,7 @@ cdef extern from "ogr_api.h":
     int OGR_F_IsFieldNull(void *feature, int n)
 
 from fiona.ogrext2 cimport *
+from fiona._err import cpl_errs
 
 cdef bint is_field_null(void *feature, int n):
     if OGR_F_IsFieldNull(feature, n):
@@ -11,17 +12,35 @@ cdef bint is_field_null(void *feature, int n):
     else:
         return False
 
-cdef void* gdal_open_vector(char* path_c, int mode):
+cdef void gdal_flush_cache(void *cogr_ds):
+    with cpl_errs:
+        GDALFlushCache(cogr_ds)
+
+cdef void* gdal_open_vector(char* path_c, int mode, drivers):
     if mode == 1:
         mode = GDAL_OF_UPDATE
     else:
         mode = GDAL_OF_READONLY
-    cdef void* cogr_ds = GDALOpenEx(path_c,
-            GDAL_OF_VECTOR | mode,
-            NULL, NULL, NULL)
+    cdef void* cogr_ds
+    cdef char **drvs = NULL
+    if drivers:
+        for name in drivers:
+            name_b = name.encode()
+            name_c = name_b
+            #log.debug("Trying driver: %s", name)
+            drv = GDALGetDriverByName(name_c)
+            if drv != NULL:
+                drvs = CSLAddString(drvs, name_c)
+    
+    flags = GDAL_OF_VECTOR | mode
+    try:
+        cogr_ds = GDALOpenEx(
+            path_c, flags, <const char *const *>drvs, NULL, NULL)
+    finally:
+        CSLDestroy(drvs)
     return cogr_ds
 
-cdef void* gdal_create(void* cogr_driver, const char *path_c):
+cdef void* gdal_create(void* cogr_driver, const char *path_c) except *:
     return GDALCreate(
         cogr_driver,
         path_c,
